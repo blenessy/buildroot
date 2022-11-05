9222e13094509b099fb2931d59d87fae07842667
Zalan Blenessy <zalan.blenessy@gmail.com>
2022-11-05 20:38:02 +0100

    flashrom - take something newer

--- package/flashrom/flashrom.mk
+++ package/flashrom/flashrom.mk
@@ -5,20 +5,20 @@
 ################################################################################
 
 FLASHROM_VERSION = 1.2
-FLASHROM_SOURCE = flashrom-v$(FLASHROM_VERSION).tar.bz2
-FLASHROM_SITE = https://download.flashrom.org/releases
+FLASHROM_SOURCE = 39b189077379a6cdd99e5ae20452fa685b94500e.tar.gz
+FLASHROM_SITE = https://github.com/flashrom/flashrom/archive
 FLASHROM_LICENSE = GPL-2.0+
 FLASHROM_LICENSE_FILES = COPYING
 
 ifeq ($(BR2_PACKAGE_LIBFTDI),y)
 FLASHROM_DEPENDENCIES += host-pkgconf libftdi
 FLASHROM_MAKE_OPTS += \
-	CONFIG_FT2232_SPI=yes \
-	CONFIG_USBBLASTER_SPI=yes
+        CONFIG_FT2232_SPI=yes \
+        CONFIG_USBBLASTER_SPI=yes
 else
 FLASHROM_MAKE_OPTS += \
-	CONFIG_FT2232_SPI=no \
-	CONFIG_USBBLASTER_SPI=no
+        CONFIG_FT2232_SPI=no \
+        CONFIG_USBBLASTER_SPI=no
 endif
 
 ifeq ($(BR2_PACKAGE_LIBUSB),y)
@@ -36,13 +36,11 @@ FLASHROM_MAKE_OPTS += CONFIG_ENABLE_LIBPCI_PROGRAMMERS=no
 endif
 
 define FLASHROM_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
-		CFLAGS="$(TARGET_CFLAGS) -DHAVE_STRNLEN" \
-		$(FLASHROM_MAKE_OPTS) -C $(@D)
+        $(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) WARNERROR=no \
+                CFLAGS="$(TARGET_CFLAGS) -DHAVE_STRNLEN" \
+                $(FLASHROM_MAKE_OPTS) -C $(@D)
 endef
 
 define FLASHROM_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(@D)/flashrom $(TARGET_DIR)/usr/sbin/flashrom
+        $(INSTALL) -m 0755 -D $(@D)/flashrom $(TARGET_DIR)/usr/sbin/flashrom
 endef
-
-$(eval $(generic-package))
